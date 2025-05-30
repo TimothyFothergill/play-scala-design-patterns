61f30276ef1279e518a173041e70cfd3e08ffa40
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2025-05-30 16:36:46 +0100

    add initial Bridge code

--- app/patterns/structuralpatterns/bridge/Bridge.scala
+++ app/patterns/structuralpatterns/bridge/Bridge.scala
@@ -1,3 +1,78 @@
 package patterns.structuralpatterns.bridge
 
-case class Bridge()
+class RPGClassWarrior extends RPGClassBridge (
+    className   = "",
+    attack      = 13,
+    defence     = 12,
+    health      = 12,
+    wisdom      = 8,
+    intellect   = 7,
+    dexterity   = 10,
+    weaponType  = Sword    
+)
+
+class RPGClassMage extends RPGClassBridge (
+    className   = "Mage",
+    attack      = 7,
+    defence     = 8,
+    health      = 10,
+    wisdom      = 12,
+    intellect   = 15,
+    dexterity   = 8,
+    weaponType  = Staff    
+)
+
+class RPGClassRanger extends RPGClassBridge (
+    className   = "Ranger",
+    attack      = 10,
+    defence     = 10,
+    health      = 8,
+    wisdom      = 12,
+    intellect   = 10,
+    dexterity   = 14,
+    weaponType  = Bow
+)
+
+class RPGClassBridge(
+    className   : String    ,
+    attack      : Int       ,
+    defence     : Int       ,
+    health      : Int       ,
+    wisdom      : Int       ,
+    intellect   : Int       ,
+    dexterity   : Int       ,
+    weaponType  : WeaponType
+) 
+
+
+object RPGClassBridge {
+
+}
+
+sealed trait WeaponType {
+    val weaponName: String
+    val strength: Int
+    val intellect: Int
+    val dexterity: Int
+}
+
+object Sword extends WeaponType {
+  override val weaponName: String = "Sword"
+  override val strength: Int = 11
+  override val intellect: Int = 0
+  override val dexterity: Int = 0
+}
+
+object Staff extends WeaponType {
+  override val weaponName: String = "Staff"
+  override val strength: Int = 0
+  override val intellect: Int = 11
+  override val dexterity: Int = 0
+}
+
+object Bow extends WeaponType {
+  override val weaponName: String = "Bow"
+  override val strength: Int = 0
+  override val intellect: Int = 0
+  override val dexterity: Int = 11
+}
